4a51ea2f34a90e6d741f9d58a2c14432aee68a0d
xiexiaojun <907762353@qq.com>
2016-05-20 17:03:25 +0800

    userService代码提交

--- NhryService/src/main/java/com/nhry/service/impl/UserServiceImpl.java
+++ NhryService/src/main/java/com/nhry/service/impl/UserServiceImpl.java
@@ -34,20 +34,8 @@ public class UserServiceImpl extends BaseService implements UserService {
 
 	@Override
 //	@Transactional(readOnly=true,propagation=Propagation.REQUIRED)
-	public int greeUser(User user) {
+	public int addUser(User user) {
 		// TODO Auto-generated method stub
-		for(int t=1;t<=10;t++){
-			User u = new User();
-//			if(t  >= 9){
-//				if(t%0==0){
-//					
-//				}
-//			}
-			u.setId(t);
-			u.setUserName("张三"+t);
-			u.setComments("备注"+t);
-			userMapper.addUser(u);
-		}
-		return 0;
+		return userMapper.addUser(user);
 	}
 }
