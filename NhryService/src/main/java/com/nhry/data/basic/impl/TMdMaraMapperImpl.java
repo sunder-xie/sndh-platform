ec929f68328eb8f569e28eca30c30b54b8036d35
chenbaizhong <86765777@qq.com>
2016-06-23 16:24:24 +0800

    增加axis2相关jar包，PI接口相关基本配置，物料接口功能代码，增加物料调度任务，明天23：59自动执行。

--- NhryService/src/main/java/com/nhry/data/basic/impl/TMdMaraMapperImpl.java
+++ NhryService/src/main/java/com/nhry/data/basic/impl/TMdMaraMapperImpl.java
@@ -6,48 +6,75 @@ import com.nhry.data.basic.dao.TMdMaraMapper;
 import com.nhry.data.basic.domain.TMdMara;
 import com.nhry.service.basic.pojo.ProductInfoExModel;
 import com.nhry.model.basic.ProductQueryModel;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import java.util.List;
 
 public class TMdMaraMapperImpl implements TMdMaraMapper {
-	private DynamicSqlSessionTemplate sqlSessionTemplate;
-	public void setSqlSessionTemplate(DynamicSqlSessionTemplate sqlSessionTemplate) {
-		this.sqlSessionTemplate = sqlSessionTemplate;
-	}
-	
-	@Override
-	public TMdMara selectProductByCode(String productCode) {
-		// TODO Auto-generated method stub
-		return this.sqlSessionTemplate.selectOne("selectProductByCode", productCode);
-	}
 
-	@Override
-	public int uptProductByCode(TMdMara record) {
-		// TODO Auto-generated method stub
-		return this.sqlSessionTemplate.update("uptProductByCode", record);
-	}
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+        this.sqlSessionFactory = sqlSessionFactory;
+    }
+
+    private SqlSessionFactory sqlSessionFactory;
+    private DynamicSqlSessionTemplate sqlSessionTemplate;
+
+    public void setSqlSessionTemplate(DynamicSqlSessionTemplate sqlSessionTemplate) {
+        this.sqlSessionTemplate = sqlSessionTemplate;
+    }
+
+    @Override
+    public TMdMara selectProductByCode(String productCode) {
+        // TODO Auto-generated method stub
+        return this.sqlSessionTemplate.selectOne("selectProductByCode", productCode);
+    }
+
+    @Override
+    public int uptProductByCode(TMdMara record) {
+        // TODO Auto-generated method stub
+        return this.sqlSessionTemplate.update("uptProductByCode", record);
+    }
 
 
-	@Override
-	public PageInfo searchProducts(ProductQueryModel smodel) {
-		// TODO Auto-generated method stub
-		return sqlSessionTemplate.selectListByPages("searchProducts",smodel, Integer.parseInt(smodel.getPageNum()), Integer.parseInt(smodel.getPageSize()));
-	}
+    @Override
+    public PageInfo searchProducts(ProductQueryModel smodel) {
+        // TODO Auto-generated method stub
+        return sqlSessionTemplate.selectListByPages("searchProducts", smodel, Integer.parseInt(smodel.getPageNum()), Integer.parseInt(smodel.getPageSize()));
+    }
 
-	@Override
-	public List<ProductInfoExModel> selectProductAndExListByCode(String productCode) {
-		return this.sqlSessionTemplate.selectList("selectProductAndExListByCode", productCode);
-	}
+    @Override
+    public List<ProductInfoExModel> selectProductAndExListByCode(String productCode) {
+        return this.sqlSessionTemplate.selectList("selectProductAndExListByCode", productCode);
+    }
 
-	@Override
-	public ProductInfoExModel selectProductAndExByCode(String productCode){
-		// TODO Auto-generated method stub
-		return this.sqlSessionTemplate.selectOne("selectProductAndExByCode", productCode);
-	}
+    @Override
+    public int insertProduct(TMdMara tMdMara) {
+        return sqlSessionTemplate.insert("insertProduct",tMdMara);
+    }
 
-	@Override
-	public int pubProductByCode(String code){
-		// TODO Auto-generated method stub
-		return this.sqlSessionTemplate.update("pubProductByCode", code);
-	}
+    @Override
+    public int updateProduct(TMdMara tMdMara) {
+        return sqlSessionTemplate.update("updateProduct",tMdMara);
+    }
+
+    @Override
+    public int isProduct(String id) {
+        return sqlSessionTemplate.selectOne("isProduct",id);
+    }
+
+    @Override
+    public ProductInfoExModel selectProductAndExByCode(String productCode) {
+        // TODO Auto-generated method stub
+        return this.sqlSessionTemplate.selectOne("selectProductAndExByCode", productCode);
+    }
+
+    @Override
+    public int pubProductByCode(String code) {
+        // TODO Auto-generated method stub
+        return this.sqlSessionTemplate.update("pubProductByCode", code);
+    }
+
+
 }
