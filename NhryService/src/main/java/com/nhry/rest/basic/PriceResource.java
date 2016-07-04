bb64b3424404bb81cc715b84b8f2d4381fb8d0e1
xiexiaojun <907762353@qq.com>
2016-07-04 16:17:52 +0800

    删除没有用的方法

--- NhryService/src/main/java/com/nhry/rest/basic/PriceResource.java
+++ NhryService/src/main/java/com/nhry/rest/basic/PriceResource.java
@@ -98,7 +98,7 @@ public class PriceResource extends BaseResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@ApiOperation(value = "/add/price/branch", response = ResponseModel.class, notes = "添加价格组与奶站关系")
-	public Response pubProductByCode(@ApiParam(required=true,name="record",value="价格组与奶站关系对象(priceType不用填写)")TMdPriceBranch record){
+	public Response pubProductByCode(@ApiParam(required=true,name="record",value="价格组与奶站关系对象")TMdPriceBranch record){
 		return convertToRespModel(MessageCode.NORMAL, null, priceService.addPriceBranch(record));
 	}
 	
@@ -118,15 +118,5 @@ public class PriceResource extends BaseResource {
 	@ApiOperation(value = "/dealers", response = TMdDealer.class, notes = "获取当前登录人所在公司下面的所有经销商")
 	public Response getDealers(){
 		return convertToRespModel(MessageCode.NORMAL, null, priceService.getDealers());
-	}
-	
-	@POST
-	@Path("/matnr/{branchNo}/{matnr}/{deliveryType}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "/matnr/{matnr}", response = TMdDealer.class, notes = "商品价格")
-	public Response getMatnrPrice(@ApiParam(required=true,name="branchNo",value="奶站编号")@PathParam("branchNo")String branchNo,
-			@ApiParam(required=true,name="matnr",value="产品编号")@PathParam("matnr")String matnr,
-			@ApiParam(required=true,name="deliveryType",value="配送类型")@PathParam("deliveryType")String deliveryType){
-		return convertToRespModel(MessageCode.NORMAL, null, priceService.getMaraPrice(branchNo, matnr, deliveryType));
 	}
 }
