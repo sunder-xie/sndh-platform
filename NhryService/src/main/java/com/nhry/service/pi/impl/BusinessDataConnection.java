d788fb8db33404f2a46e0a23d90f1abfb1d5b447
chenbaizhong <86765777@qq.com>
2016-07-05 08:43:15 +0800

    产品主数据优化，及要货单接口实现

--- NhryService/src/main/java/com/nhry/service/pi/impl/BusinessDataConnection.java
+++ NhryService/src/main/java/com/nhry/service/pi/impl/BusinessDataConnection.java
@@ -4,6 +4,7 @@ import com.nhry.utils.PIPropertitesUtil;
 import com.nhry.webService.OptionManager;
 import com.nhry.webService.client.businessData.ZT_BusinessData_MaintainServiceCallbackHandler;
 import com.nhry.webService.client.businessData.ZT_BusinessData_MaintainServiceStub;
+import com.nhry.webService.client.businessData.functions.*;
 import com.nhry.webService.client.masterData.ZT_MasterDataQueryServiceStub;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.client.Options;
@@ -30,40 +31,40 @@ public class BusinessDataConnection {
 
     public static String RequisitionCreate(){
 
-        ZT_BusinessData_MaintainServiceStub.ZSD_REQUISITION_CREATE_RFC rfc = new ZT_BusinessData_MaintainServiceStub.ZSD_REQUISITION_CREATE_RFC();
-        ZT_BusinessData_MaintainServiceStub.ZSD_REQ_EKKO ekko = new ZT_BusinessData_MaintainServiceStub.ZSD_REQ_EKKO();
-        ZT_BusinessData_MaintainServiceStub.COMP_CODE_type1 comp_code_type1 = new ZT_BusinessData_MaintainServiceStub.COMP_CODE_type1();
+        ZSD_REQUISITION_CREATE_RFC rfc = new ZSD_REQUISITION_CREATE_RFC();
+        ZSD_REQ_EKKO ekko = new ZSD_REQ_EKKO();
+        COMP_CODE_type1 comp_code_type1 = new COMP_CODE_type1();
         comp_code_type1.setCOMP_CODE_type0("");
         ekko.setCOMP_CODE(comp_code_type1);
 
-        ZT_BusinessData_MaintainServiceStub.DOC_TYPE_type1 doc_type_type1 = new ZT_BusinessData_MaintainServiceStub.DOC_TYPE_type1();
+        DOC_TYPE_type1 doc_type_type1 = new DOC_TYPE_type1();
         doc_type_type1.setDOC_TYPE_type0("");
         ekko.setDOC_TYPE(doc_type_type1);
         ParsePosition pos = new ParsePosition(8);
         String dateString = formatter.format(new Date());
-        ZT_BusinessData_MaintainServiceStub.Date date = new ZT_BusinessData_MaintainServiceStub.Date();
+        com.nhry.webService.client.businessData.functions.Date date = new com.nhry.webService.client.businessData.functions.Date();
         date.setObject(formatter.parse(dateString,pos));
         ekko.setI_DATUM(date);
 
-        ZT_BusinessData_MaintainServiceStub.PUR_GROUP_type1 pur_group_type1 = new ZT_BusinessData_MaintainServiceStub.PUR_GROUP_type1();
+        PUR_GROUP_type1 pur_group_type1 = new PUR_GROUP_type1();
         pur_group_type1.setPUR_GROUP_type0("");
         ekko.setPUR_GROUP(pur_group_type1);
 
-        ZT_BusinessData_MaintainServiceStub.SUPPL_PLNT_type1 suppl_plnt_type1 = new ZT_BusinessData_MaintainServiceStub.SUPPL_PLNT_type1();
+        SUPPL_PLNT_type1 suppl_plnt_type1 = new SUPPL_PLNT_type1();
         suppl_plnt_type1.setSUPPL_PLNT_type0("");
         ekko.setSUPPL_PLNT(suppl_plnt_type1);
 
-        ZT_BusinessData_MaintainServiceStub.PURCH_ORG_type1 purch_org_type1 = new ZT_BusinessData_MaintainServiceStub.PURCH_ORG_type1();
+        PURCH_ORG_type1 purch_org_type1 = new PURCH_ORG_type1();
         purch_org_type1.setPURCH_ORG_type0("");
         ekko.setPURCH_ORG(purch_org_type1);
 
         rfc.setI_EKKO(ekko);
 
-        ZT_BusinessData_MaintainServiceStub.IT_ITEM_type0 it_item_type0 = new ZT_BusinessData_MaintainServiceStub.IT_ITEM_type0();
+        IT_ITEM_type0 it_item_type0 = new IT_ITEM_type0();
 
 
 
-        ZT_BusinessData_MaintainServiceStub.ZSSD00019 zssd00019 = new ZT_BusinessData_MaintainServiceStub.ZSSD00019();
+        ZSSD00019 zssd00019 = new ZSSD00019();
 //        zssd00019.setEXT_RFX_ITEM();
 //        zssd00019.setEXT_RFX_NUMBER();
 //        zssd00019.setLGORT();
