f69e2f24bc8154877f59b9c87ea4f3a66960f6bb
xiexiaojun <907762353@qq.com>
2016-07-12 19:35:13 +0800

    添加产品查询产品编码、规格接口并修改查询有效产品接口

--- NhryService/src/main/java/com/nhry/service/basic/dao/ProductService.java
+++ NhryService/src/main/java/com/nhry/service/basic/dao/ProductService.java
@@ -4,6 +4,7 @@ import com.github.pagehelper.PageInfo;
 import com.nhry.data.basic.domain.TMdMara;
 import com.nhry.data.basic.domain.TMdMaraEx;
 import com.nhry.model.basic.ProductQueryModel;
+import com.nhry.service.basic.pojo.BotType;
 import com.nhry.service.basic.pojo.ProductInfoExModel;
 
 import  java.util.*;
@@ -73,4 +74,10 @@ public interface ProductService {
     PageInfo getBranchSaleMaras(ProductQueryModel pm);
 
     List<TMdMara>  getProductByCodeOrName(String product);
+    
+    /**
+     * 获取当前销售组织下产品与奶瓶规格数据
+     * @return
+     */
+    Map<String,String> getMataBotTypes();
 }
