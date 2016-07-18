4e6a6979eefe8dd99903a9ad605a2c1365c647e2
chenbaizhong <86765777@qq.com>
2016-07-18 18:57:03 +0800

    奶站日报表和用户接口修改

---
+++ NhryService/src/main/java/com/nhry/service/statistics/dao/BranchInfoService.java
@@ -0,0 +1,11 @@
+package com.nhry.service.statistics.dao;
+
+import com.github.pagehelper.PageInfo;
+import com.nhry.model.statistics.BranchInfoModel;
+
+/**
+ * Created by cbz on 7/18/2016.
+ */
+public interface BranchInfoService {
+    public PageInfo branchDayInfo(BranchInfoModel model);
+}
