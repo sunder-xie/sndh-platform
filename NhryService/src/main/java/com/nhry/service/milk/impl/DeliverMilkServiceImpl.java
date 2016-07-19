3995bf0f65bad7443d254132ec139d8fb0351371
wei yun san <451452830@qq.com>
2016-07-19 16:59:25 +0800

    订单维护修改

--- NhryService/src/main/java/com/nhry/service/milk/impl/DeliverMilkServiceImpl.java
+++ NhryService/src/main/java/com/nhry/service/milk/impl/DeliverMilkServiceImpl.java
@@ -436,20 +436,19 @@ public class DeliverMilkServiceImpl extends BaseService implements DeliverMilkSe
 	{
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		if(userSessionService.getCurrentUser().getBranchNo()==null)throw new ServiceException(MessageCode.LOGIC_ERROR,"登陆人没有奶站，非奶站人员无法创建路单!");
-		if(tDispOrderMapper.selectTodayDispOrderByBranchNo(userSessionService.getCurrentUser().getBranchNo()).size()>0)throw new ServiceException(MessageCode.LOGIC_ERROR,"本日该奶站已经创建过路单!");
+//		if(tDispOrderMapper.selectTodayDispOrderByBranchNo(userSessionService.getCurrentUser().getBranchNo()).size()>0)throw new ServiceException(MessageCode.LOGIC_ERROR,"本日该奶站已经创建过路单!");
 		List<TPreOrder> empNos = tPreOrderMapper.selectDispNoByGroup(userSessionService.getCurrentUser().getBranchNo());
 		TDispOrder dispOrder = null;
 		List<TDispOrderItem> dispEntries = null;
-		Date date = null;
+		Date date = afterDate(new Date(),1);
 		Map<String,String> productMap = productService.getMataBotTypes();
 		for(TPreOrder order : empNos){
-			date = new Date();
 			dispOrder = new TDispOrder();
 			dispEntries = new ArrayList<TDispOrderItem>();
 			int totalQty = 0;
 			BigDecimal totalAmt = new BigDecimal("0.00");
 			//生成一条路线，一个配送时段的路单
-			List<TOrderDaliyPlanItem> daliyPlans = tOrderDaliyPlanItemMapper.selectbyDispLineNo(order.getEmpNo(),format.format(new Date()),order.getOrderType());
+			List<TOrderDaliyPlanItem> daliyPlans = tOrderDaliyPlanItemMapper.selectbyDispLineNo(order.getEmpNo(),format.format(date),order.getOrderType());
 			
 			if(daliyPlans == null || daliyPlans.size() <= 0)continue;
 				
