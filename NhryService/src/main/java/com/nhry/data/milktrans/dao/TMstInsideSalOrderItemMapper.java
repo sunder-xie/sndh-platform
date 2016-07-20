fdc65f78a32032b8ce09c9266ec6f090eb3814f0
gongnol <979164557@qq.com>
2016-07-20 13:11:37 +0800

    添加 获取内部销售订单 和详情的接口

--- NhryService/src/main/java/com/nhry/data/milktrans/dao/TMstInsideSalOrderItemMapper.java
+++ NhryService/src/main/java/com/nhry/data/milktrans/dao/TMstInsideSalOrderItemMapper.java
@@ -1,6 +1,8 @@
 package com.nhry.data.milktrans.dao;
 
+import com.github.pagehelper.PageInfo;
 import com.nhry.data.milktrans.domain.TMstInsideSalOrderItem;
+import com.nhry.model.milktrans.InSideSalOrderDetailSearchModel;
 
 /**
  * Created by gongjk on 2016/6/30.
@@ -9,4 +11,6 @@ public interface TMstInsideSalOrderItemMapper {
     int delInSalOrderItemByOrderNo(String insOrderNo);
 
     int insertOrderItem(TMstInsideSalOrderItem item);
+
+    PageInfo getInsideSalOrderDetail(InSideSalOrderDetailSearchModel sModel);
 }
