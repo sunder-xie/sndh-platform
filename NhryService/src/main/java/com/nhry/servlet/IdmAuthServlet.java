5450feb2282304cb10f51d7c728c9fb43a7503a8
xiexiaojun <907762353@qq.com>
2016-07-23 17:21:17 +0800

    修改idm auth servlet

--- NhryService/src/main/java/com/nhry/servlet/IdmAuthServlet.java
+++ NhryService/src/main/java/com/nhry/servlet/IdmAuthServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.nhry.utils.APIHttpClient;
 import com.nhry.utils.EnvContant;
@@ -27,30 +29,36 @@ public class IdmAuthServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// 获取临时令牌
 		System.out.println("----------code-获取临时令牌------------");
-		String code = request.getParameter("code");
-		if (!StringUtils.isEmpty(code)) {
-			Map<String,Object> attrs = new HashMap<String,Object>();
-			attrs.put("client_id", EnvContant.getSystemConst("client_id"));
-			attrs.put("client_secret", EnvContant.getSystemConst("client_secret"));
-			attrs.put("grant_type", EnvContant.getSystemConst("grant_type"));
-			attrs.put("redirect_uri", EnvContant.getSystemConst("redirect_uri"));
-			attrs.put("code", code);
-			String access_token = HttpUtils.request(EnvContant.getSystemConst("auth_token"), attrs);
-			System.out.println("-----access_token-------"+access_token);
-			if(!StringUtils.isEmpty(access_token)){
-				attrs.clear();
-				attrs.put("access_token", access_token);
-				String msg = HttpUtils.request(EnvContant.getSystemConst("auth_profile"), attrs);
-				System.out.println("-----msg-------"+msg);
+		try {
+			String code = request.getParameter("code");
+			if (!StringUtils.isEmpty(code)) {
+				Map<String,Object> attrs = new HashMap<String,Object>();
+				attrs.put("client_id", EnvContant.getSystemConst("client_id"));
+				attrs.put("client_secret", EnvContant.getSystemConst("client_secret"));
+				attrs.put("grant_type", EnvContant.getSystemConst("grant_type"));
+				attrs.put("redirect_uri", EnvContant.getSystemConst("redirect_uri"));
+				attrs.put("code", code);
+				String access_token = HttpUtils.request(EnvContant.getSystemConst("auth_token"), attrs);
+				System.out.println("-----access_token-------"+access_token);
+				if(!StringUtils.isEmpty(access_token)){
+					attrs.clear();
+					JSONObject json = new JSONObject(access_token);
+					attrs.put("access_token", json.getString("access_token"));
+					String msg = HttpUtils.request(EnvContant.getSystemConst("auth_profile"), attrs);
+					System.out.println("-----msg-------"+msg);
+				}
+			} else {
+				//跳转到登录页面
+				response.sendRedirect(EnvContant.getSystemConst("authurl")
+						+ "?client_id=" + EnvContant.getSystemConst("client_id")
+						+ "&redirect_uri="
+						+ EnvContant.getSystemConst("redirect_uri")
+						+ "&response_type="
+						+ EnvContant.getSystemConst("response_type"));
 			}
-		} else {
-			//跳转到登录页面
-			response.sendRedirect(EnvContant.getSystemConst("authurl")
-					+ "?client_id=" + EnvContant.getSystemConst("client_id")
-					+ "&redirect_uri="
-					+ EnvContant.getSystemConst("redirect_uri")
-					+ "&response_type="
-					+ EnvContant.getSystemConst("response_type"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
