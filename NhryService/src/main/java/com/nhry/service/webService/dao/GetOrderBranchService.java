f54396d12288cc2234cd823cf9b17eb7d1f0fa25
gongnol <979164557@qq.com>
2016-07-27 17:33:21 +0800

    修改回瓶

---
+++ NhryService/src/main/java/com/nhry/service/webService/dao/GetOrderBranchService.java
@@ -0,0 +1,10 @@
+package com.nhry.service.webService.dao;
+
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ * Created by gongjk on 2016/7/27.
+ */
+public interface GetOrderBranchService {
+    public JSONObject getOrderBranch(JSONObject obj);
+}
