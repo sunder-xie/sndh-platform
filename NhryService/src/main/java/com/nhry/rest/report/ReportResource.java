ea24d1c27e5e4eb2bc2d1a0a9a65d942d9c3deaf
chenbaizhong <86765777@qq.com>
2016-07-28 11:18:06 +0800

    导出excel

--- NhryService/src/main/java/com/nhry/rest/report/ReportResource.java
+++ NhryService/src/main/java/com/nhry/rest/report/ReportResource.java
@@ -1,6 +1,7 @@
 package com.nhry.rest.report;
 
 import com.nhry.common.auth.UserSessionService;
+import com.nhry.common.exception.MessageCode;
 import com.nhry.data.auth.domain.TSysUser;
 import com.nhry.data.basic.domain.TMdAddress;
 import com.nhry.data.basic.domain.TMdBranch;
@@ -8,12 +9,15 @@ import com.nhry.data.order.domain.TPreOrder;
 import com.nhry.model.order.CollectOrderModel;
 import com.nhry.model.order.OrderCreateModel;
 import com.nhry.model.order.ProductItem;
+import com.nhry.rest.BaseResource;
 import com.nhry.service.basic.dao.BranchService;
 import com.nhry.service.order.dao.OrderService;
+import com.nhry.utils.CodeGeneratorUtil;
 import com.sun.jersey.spi.resource.Singleton;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
+import org.apache.log4j.Logger;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -47,8 +51,10 @@ import java.util.List;
 @Singleton
 @Scope("request")
 @Api(value = "/report", description = "导出")
-public class ReportResource {
+public class ReportResource extends BaseResource{
+    private static Logger logger = Logger.getLogger(ReportResource.class);
     private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private static SimpleDateFormat format1 = new SimpleDateFormat("yyMMdd");
     @Autowired
     private OrderService orderService;
     @Autowired
@@ -59,22 +65,24 @@ public class ReportResource {
     @Path("/reportCollect")
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "/reportCollect", response = OrderCreateModel.class, notes = "根据订单编号导出收款信息")
-    public void reportCollect(@ApiParam(required = true, name = "orderCode", value = "订单编号") @QueryParam("orderCode") String orderCode, @Context HttpServletRequest request, @Context HttpServletResponse response) {
+    public Response reportCollect(@ApiParam(required = true, name = "orderCode", value = "订单编号") @QueryParam("orderCode") String orderCode, @Context HttpServletRequest request, @Context HttpServletResponse response) {
         CollectOrderModel collect = orderService.queryCollectByOrderNo(orderCode);
         TSysUser user = userSessionService.getCurrentUser();
         String url = request.getServletContext().getRealPath("/");
+        logger.info("realPath："+url);
         TMdAddress address = collect.getAddress();
         TPreOrder order = collect.getOrder();
         TMdBranch branch = branchService.selectBranchByNo(order.getBranchNo());
         List<ProductItem> productItems =collect.getEntries();
+        String outUrl = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
         try {
-            File file = new File(url + File.separator + "WEB-INF" + File.separator + "reportTemplate" + File.separator + "CollectOrderTemplate.xlsx");    //审批单
+            File file = new File(url +  File.separator + "report"+ File.separator + "template" + File.separator + "CollectOrderTemplate.xlsx");    //审批单
             FileInputStream input = new FileInputStream(file);
             XSSFWorkbook workbook = new XSSFWorkbook(new BufferedInputStream(input));
             XSSFSheet sheet = workbook.getSheetAt(0);
             XSSFRow row = sheet.getRow(3);
             XSSFCell cell = row.getCell(1);
-            
+
             cell.setCellValue("配送奶站："+order.getBranchName());
             cell = row.getCell(4);
            // 订奶起止日期：2016-7-21—  2016-8-23
@@ -109,7 +117,6 @@ public class ReportResource {
                     cell.setCellValue(item.getBasePrice().toString());
                     cell = row.getCell(8);
                     cell.setCellValue(item.getQty());
-
                     cell = row.getCell(10);
                     cell.setCellValue(item.getTotalPrice().toString());
                     if(item.getTotalPrice()!=null)
@@ -130,21 +137,32 @@ public class ReportResource {
             row = sheet.getRow(18);
             cell = row.getCell(1);
             cell.setCellValue("打印日期："+ format.format(new Date()));
-
-            response.setContentType("multipart/form-data");
-            response.setHeader("Content-Disposition", "filename="+new String("collectOrder.xlsx".getBytes(),"iso-8859-1"));
-            OutputStream fileOut = response.getOutputStream();
-            workbook.write(fileOut);
-            fileOut.flush();
-            fileOut.close();
+            String fname = CodeGeneratorUtil.getCode();
+            String rq = format1.format(new Date(new Date().getTime() - 24 * 60 * 60 * 1000));
+            String filePath = url +  File.separator + "report"+ File.separator + "export";
+            File delFiles = new File(filePath);
+            if(delFiles.isDirectory()){
+                for(File del : delFiles.listFiles()){
+                    if(del.getName().contains(rq)){
+                        del.delete();
+                    }
+                }
+            }
+            File export = new File(url +  File.separator + "report"+ File.separator + "export" + File.separator + fname + "CollectOrder.xlsx");
+            FileOutputStream stream = new FileOutputStream(export);
+            workbook.write(stream);
+            stream.flush();
+            stream.close();
 
 //            String mt = new MimetypesFileTypeMap().getContentType(targetFilePath);
 //            return Response
 //                    .ok(targetFilePath, mt)
 //                    .header("Content-disposition","attachment;filename=" + targetFilePath.getName())
 //                    .header("ragma", "No-cache").header("Cache-Control", "no-cache").build();
+            outUrl = outUrl + "/report/export/" + fname + "CollectOrder.xlsx";
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return convertToRespModel(MessageCode.NORMAL,null,outUrl);
     }
 }
