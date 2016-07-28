2362a5a0a51555301c7bae4a23f62fbea03a91cf
xiexiaojun <907762353@qq.com>
2016-07-28 13:56:31 +0800

    修改资源文件加载标示

--- NhryService/src/main/java/com/nhry/utils/EnvContant.java
+++ NhryService/src/main/java/com/nhry/utils/EnvContant.java
@@ -22,12 +22,13 @@ public class EnvContant {
 	private static final String APP_DEV_CONFIG = "env/app-dev.xml";
 	static Logger log = Logger.getLogger(EnvContant.class);
 	private static Map<String, String> configs = new HashMap<String, String>();
+	private static final String SYS_MODE="product";
 
 	static {
 		try {
 			SAXReader read = new SAXReader();
 			String appEnvConfig = APP_DEV_CONFIG;
-			if ("produce".equals(SysContant.getSystemConst("app_mode"))) {
+			if (SYS_MODE.equals(SysContant.getSystemConst("app_mode"))) {
 				appEnvConfig = APP_PROD_CONFIG;
 			}
 			// 获取根下面的 config,xml文件 进行加载读取
