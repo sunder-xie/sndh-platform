5fedf37922cea8f1bd16aeae34b9d82baef53a18
gongnol <979164557@qq.com>
2016-10-31 14:54:29 +0800

    修改，将创建订单时的 已支付订单payDateStr字段验证提出来

--- NhryService/src/main/java/com/nhry/data/basic/domain/DailOrderLogEnum.java
+++ NhryService/src/main/java/com/nhry/data/basic/domain/DailOrderLogEnum.java
@@ -6,7 +6,6 @@ import java.io.Serializable;
  * Created by gongjk on 2016/10/24.
  */
 public class DailOrderLogEnum implements Serializable {
-    public final static  String REACH_TIME = "修改配送时间";
     public final static  String UPT_MATNR = "修改产品";
     public final static  String UPT_QTY = "修改数量";
     public final static  String UPT_MATNR_QTY = "修改产品和数量";
