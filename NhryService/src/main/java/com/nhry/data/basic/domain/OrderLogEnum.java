fb812cbf8bc0b56b8daf98839f1336a3548a403b
gongnol <979164557@qq.com>
2016-11-01 11:10:06 +0800

    补提 订单日志

--- NhryService/src/main/java/com/nhry/data/basic/domain/OrderLogEnum.java
+++ NhryService/src/main/java/com/nhry/data/basic/domain/OrderLogEnum.java
@@ -6,15 +6,16 @@ import java.io.Serializable;
  * Created by gongjk on 2016/10/24.
  */
 public class OrderLogEnum implements Serializable {
-    public final static  String CREATE_ORDER = "创建订单";
+    //public final static  String CREATE_ORDER = "创建订单";
     public final static  String MEMO_TXT = "备注";
     public final static  String ADD_PRODUCT = "新增产品";
     public final static  String CHANGE_PRODUCT = "变更品种";
     public final static  String CHANGE_QTY = "变更数量";
     public final static  String CHANGE_RULE_TYPE = "变更周期";
-    public final static  String CHANGE_RULE = "变更配送规律";
+   // public final static  String CHANGE_RULE = "变更配送规律";
     public final static  String STOP_ORDER = "停订";
     public final static  String BACK_ORDER = "退订";
+    public final static  String RESUME_ORDER = "复订";
     public final static  String DH_BACK_ORDER = "电商退订";
     public final static  String DEL_ITEM = "删除";
     public final static  String CHANGE_EMP = "送奶员替换";
