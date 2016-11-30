5ee984ce47fe0c7051afa7ace1c32e081dc6edb3
guanhua <149976419@qq.com>
2016-11-30 14:09:35 +0800

    update

--- NhryService/src/main/java/com/nhry/service/order/impl/OrderServiceImpl.java
+++ NhryService/src/main/java/com/nhry/service/order/impl/OrderServiceImpl.java
@@ -4659,9 +4659,9 @@ public class OrderServiceImpl extends BaseService implements OrderService {
 		/*	Map<String,Integer> dayEntrMap = new HashMap<String,Integer>();
 		//dayEntrMap.put("planItemNo",0);*/
 		//allDayItems.stream().filter(e->!"30".equals(e.getStatus())).count();
-		System.out.println("开始执行更新日订单剩余金额，取出所有的日订单数量为"+allDayItems.size());
+		System.out.println(order.getOrderNo()+"开始执行更新日订单剩余金额，取出所有的日订单数量为"+allDayItems.size());
 		if(allDayItems!=null && allDayItems.size()>0){
-			allDayItems.stream().filter(e->StringUtils.isBlank(e.getPromotionFlag())).filter(e->!"30".equals(e.getStatus())).forEach(item->{
+			allDayItems.stream().filter(e->!"30".equals(e.getStatus())).forEach(item->{
 				initMap.replace("initAmt",initMap.get("initAmt").subtract(item.getAmt()));
 					TOrderDaliyPlanItem plan = new TOrderDaliyPlanItem();
 					plan.setDispDate(item.getDispDate());//配送日期
@@ -5102,7 +5102,7 @@ public class OrderServiceImpl extends BaseService implements OrderService {
 					cloneUseAmt = cloneUseAmt.add(item.getAmt());
 				}
 				initMap.replace("initAmt",initMap.get("initAmt").subtract(item.getAmt()));
-				if(StringUtils.isBlank(item.getPromotionFlag()) && item.getRemainAmt().compareTo(initMap.get("initAmt"))!=0){
+				if(item.getRemainAmt().compareTo(initMap.get("initAmt"))!=0){
 					TOrderDaliyPlanItem plan = new TOrderDaliyPlanItem();
 					plan.setDispDate(item.getDispDate());//配送日期
 					plan.setOrderNo(item.getOrderNo());//订单编号
