33ea703a073f79a7256536b4219764bf46c3787d
guanhua <149976419@qq.com>
2016-12-21 16:17:10 +0800

    年卡补偿单据-接口

--- NhryService/src/main/java/com/nhry/data/order/dao/TYearCardCompOrderMapper.java
+++ NhryService/src/main/java/com/nhry/data/order/dao/TYearCardCompOrderMapper.java
@@ -1,6 +1,9 @@
 package com.nhry.data.order.dao;
 
 import com.nhry.data.order.domain.TMstYearCardCompOrder;
+import com.nhry.model.statistics.ExtendBranchInfoModel;
+
+import java.util.List;
 
 /**
  * Created by gongjk on 2016/12/14.
@@ -8,4 +11,6 @@ import com.nhry.data.order.domain.TMstYearCardCompOrder;
 public interface TYearCardCompOrderMapper {
     //插入  年卡折扣补偿报表
     int addYearCardCompOrder(TMstYearCardCompOrder yOrder);
+
+    List<TMstYearCardCompOrder> selectYearCardCompensateList(ExtendBranchInfoModel model);
 }
